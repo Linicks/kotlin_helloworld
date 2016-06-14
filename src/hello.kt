7aa16401277e41da3f51d2ee1074b5f2328c6597
Nick Pavlica <Linicks@gmail.com>
2016-06-14 16:55:33 -0600

    Update code and comment.

--- src/hello.kt
+++ src/hello.kt
@@ -1,8 +1,8 @@
 /**
- * This is the most basic example of Hello World in Kotlin.
+ * This is a basic example of Hello World in Kotlin. To learn more,
+ * you can visit the Kotlin website at: https://kotlinlang.org/
  */
 
-
 fun main(args : Array<String>) {
-    println("Hello, world! My Name is Nick")
+    println("Hello World, My Name is Kotlin!")
 }
